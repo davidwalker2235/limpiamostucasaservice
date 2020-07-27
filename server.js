cbd4b4fba9fead0c9197b6cbbf3910f01aa988f3
davidwalker2235@gmail.com <davidwalker2235@gmail.com>
2020-07-27 13:31:15 +0200

    new project

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
-const express = require("express");
+// @ts-check
+var util = require('./util');
+const express = require('express');
+const http = require('http');
+const url = require('url');
+var cookieParser = require('cookie-parser');
+const request = require('request');
 
-const server = express();
-server.listen(3000, () => {
-    console.log("El servidor está inicializado en el puerto 3000");
-});
+async function main() {
+  // Azure App Service will set process.env.port for you, but we use 3000 in development.
+  const PORT = process.env.PORT || 3000;
+  // Create the express routes
+  let app = express();
+  app.use(express.static('public'));
+  app.use(cookieParser());
 
-server.get('/', function (req, res) {
+  app.get('/', function (req, res) {
     res.send('Saludos desde express');
-});
+  });
 
-server.get('/hola', function (req, res) {
-    res.send('[GET]Saludos desde express');
-});
+  app.get('/hola', function (req, res) {
+    res.send('[GET]hola desde express');
+  });
+
+  app.get('/api/metadata', async (req, res) => {
+    if (req.cookies.loginsession) {
+      let tryappserviceendpoint = (process.env['APPSETTING_TRYAPPSERVICE_URL'] || 'https://tryappservice.azure.com') + '/api/vscoderesource';
+      const options = {
+        url: tryappserviceendpoint,
+        headers: {
+          cookie: 'loginsession=' + req.cookies.loginsession
+        }
+      };
+
+      const x = request(options);
+      x.pipe(res);
+    }
+    else {
+      res.end(404);
+    }
+  });
+
+  // Create the HTTP server.
+  let server = http.createServer(app);
+  server.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
+
+main();
